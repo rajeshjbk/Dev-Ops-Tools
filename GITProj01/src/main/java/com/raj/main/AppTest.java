b731bcd0e619350cb44fec3116cee549c735ca66
Rajesh kumar <rajeshkumarkarri3434@gmail.com>
2025-11-30 21:42:16 +0530

    Dev1 doing basic work

---
+++ GITProj01/src/main/java/com/raj/main/AppTest.java
@@ -0,0 +1,12 @@
+package com.raj.main;
+
+import com.raj.service.ArithmeticOperations;
+
+public class AppTest {
+
+	public static void main(String[] args) {
+		
+		ArithmeticOperations ops = new ArithmeticOperations();
+		System.out.println("Result is : "+ops.sum(10,20));
+	}
+}
---
+++ GITProj01/src/main/java/com/raj/service/ArithmeticOperations.java
@@ -0,0 +1,9 @@
+package com.raj.service;
+
+public class ArithmeticOperations {
+
+	public int sum(int x, int y) {
+		
+		return x+y;
+	}
+}
