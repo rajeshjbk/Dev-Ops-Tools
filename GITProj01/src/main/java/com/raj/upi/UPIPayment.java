2cae8df12e05f193595d56ee3f91093bc61e007b
Rajesh kumar <rajeshkumarkarri3434@gmail.com>
2025-11-30 23:52:33 +0530

    final commit

--- GITProj01/src/main/java/com/raj/upi/UPIPayment.java
+++ GITProj01/src/main/java/com/raj/upi/UPIPayment.java
@@ -7,7 +7,6 @@ public class UPIPayment {
 		return amount+" amount is transfered to mobile number :: "+mobileNo+" Using BharatPay";
 	}
 	
-
 	public String doUPIPaymentByAcno(long accNo, float amount) {
 		
 		return amount+" amount is transfered to account number :: "+accNo+" Using BharatPay";
