5f223f97ce150ffff08f1b75dd9db290cfe2ddd8
Rajesh kumar <rajeshkumarkarri3434@gmail.com>
2025-11-30 14:56:38 +0530

    commit

---
+++ MavenProj01/src/main/java/com/raj/main/App.java
@@ -0,0 +1,15 @@
+package com.raj.main;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+    	System.out.println("App.main()");
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println("result is ::"+c);
+        System.out.println(System.getProperty("java.version"));
+    }
+}
