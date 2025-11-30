eb95fff905b8ac3bc32fac2c6479084fb046e733
Rajesh kumar <rajeshkumarkarri3434@gmail.com>
2025-11-30 21:46:01 +0530

    TL improvement

--- GITProj01/src/main/java/com/raj/main/AppTest.java
+++ GITProj01/src/main/java/com/raj/main/AppTest.java
@@ -7,6 +7,7 @@ public class AppTest {
 	public static void main(String[] args) {
 		
 		ArithmeticOperations ops = new ArithmeticOperations();
-		System.out.println("Result is : "+ops.sum(10,20));
+		System.out.println("Result (Sum) is : "+ops.sum(10,20));
+		System.out.println("Result (Sub) is : "+ops.sub(40,20));
 	}
 }
--- GITProj01/src/main/java/com/raj/service/ArithmeticOperations.java
+++ GITProj01/src/main/java/com/raj/service/ArithmeticOperations.java
@@ -3,7 +3,13 @@ package com.raj.service;
 public class ArithmeticOperations {
 
 	public int sum(int x, int y) {
-		
+
 		return x+y;
 	}
+
+	public int sub(int x, int y) {
+
+		return x-y;
+	}
+
 }
