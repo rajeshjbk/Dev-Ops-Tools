d59dcbe31e0dc657680831d5a1c8a10f951e967c
Rajesh kumar <rajeshkumarkarri3434@gmail.com>
2025-11-30 15:04:58 +0530

    commit

---
+++ MavenProj06-BootApp/src/main/java/com/raj/MavenProj06BootAppApplication.java
@@ -0,0 +1,13 @@
+package com.raj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenProj06BootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenProj06BootAppApplication.class, args);
+	}
+
+}
