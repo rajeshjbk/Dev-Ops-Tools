4176c1d4c4f66b452c9c79963692bcdc7128cc6f
Rajesh kumar <rajeshkumarkarri3434@gmail.com>
2025-12-10 14:12:16 +0530

    first-commit

---
+++ DockerSBWebApplication/src/main/java/com/raj/main/DockerSbWebApplication.java
@@ -0,0 +1,13 @@
+package com.raj.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSbWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSbWebApplication.class, args);
+	}
+
+}
---
+++ DockerSBWebApplication/src/test/java/com/raj/main/DockerSbWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raj.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSbWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
